ee731644f6d72c767871384cbcbcfe86d276a802
User1 <ac3r>
2019-05-08 12:43:18 +0530

    Add project files.

---
+++ BufferMgmt.Web/Extensions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BufferMgmt.Web.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //Configure CORS
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        //IIS Configuration as Part of .NET Core Configuration
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
